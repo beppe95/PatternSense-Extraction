2fc7fa62940517f4644def3253577f35185228f6
beppe95 <37592014+beppe95@users.noreply.github.com>
2020-03-26 18:47:15 +0100

    BabelNet_dict: - Cleaned e lemmatized each lemma for each semagram occurence

--- parsing/utils.py
+++ parsing/utils.py
@@ -12,7 +12,7 @@ from nltk import WordNetLemmatizer
 from data_structure.SemagramAnnotation import SemagramAnnotation
 from data_structure.Sense import Sense
 
-file = 'activity'
+file = 'accessory'
 SEMAGRAM_PATH = Path(dirname(dirname(__file__))) / 'semagram_base.xml'
 SLOT_PATH = Path(dirname(dirname(__file__))) / 'my_whoosh' / 'xml_slots' / Path(f'{file}.xml')
 translator = str.maketrans('', '', string.punctuation)
@@ -84,14 +84,10 @@ def clean_sentence(sentence: str):
     return ' '.join(cleaned_sentence)
 
 
-"""
-<pattern direction="&lt;L_&gt;">boat that could only be used in large</pattern>
-Possiamo notare che large è relativo a cosa c’è dopo, e non a boat. Per “scartare” questi casi, 
-potremmo usare delle windows (that could only be used in -> sono 6 token), o meccanismi più “clever”
-"""
-
-
 def get_patterns():
+    with open('babel_dict.pkl', mode='rb') as babel_file:
+        babel_dict = pickle.load(babel_file)
+
     with open(SLOT_PATH, mode='rb') as slot_file:
         xml_parser = etree.XMLParser(encoding='utf-8')
         extraction = etree.parse(slot_file, xml_parser).getroot()
@@ -99,60 +95,95 @@ def get_patterns():
     pattern_root = etree.Element("patterns", slot=file)
 
     window = 8
-    patterns_list = []
+    match_list = []
     for hits in extraction:
-        concept_name, concept_id = hits.attrib["name_1"], hits.attrib["babelsynset_1"]
-        filler_name, filler_id = hits.attrib['name_2'], hits.attrib['babelsynset_2']
+
+        concept_name, concept_id = hits.get("name_1").lower(), hits.get("babelsynset_1")
+        filler_name, filler_id = hits.get('name_2').lower(), hits.get('babelsynset_2')
+
         for hit in hits:
             sentence = clean_sentence(hit[1].text)
             matches = re.findall(rf'\b{concept_name}\b.*?\b{filler_name}\b'
                                  rf'|\b{filler_name}\b.*?\b{concept_name}\b',
                                  sentence, overlapped=True)
             if not matches:
-                pass
-                # print(f'Concept:{concept_name}, Filler:{filler_name} \nSentence:{sentence}\n')
+                splitted_sentence = sentence.split()
+                set_sentence = set(splitted_sentence)
+
+                try:
+                    splitted_sentence.index(concept_name)
+                except ValueError:
+                    concept_synsets = babel_dict[concept_id]
+
+                    intersect_concept_lemmas = concept_synsets.intersection(set_sentence)
+                    if intersect_concept_lemmas:
+                        concept_name = next(iter(intersect_concept_lemmas))
+
+                try:
+                    splitted_sentence.index(filler_name)
+                except ValueError:
+                    # filler_synsets = babel_dict[filler_id]
+                    filler_synsets = set([' '.join(lemmatizer.lemmatize(sense.lower()).split('_'))
+                                          for sense in
+                                          ['autumn season', 'autumn', 'falltime', 'autumntime season', 'autumn/ fall',
+                                           'fall',
+                                           'autumntime', 'mid-fall', 'fall season']])
+
+                    intersect_filler_lemmas = filler_synsets.intersection(set_sentence)
+                    if intersect_filler_lemmas:
+                        filler_name = next(iter(intersect_filler_lemmas))
+
+                matches = re.findall(rf'\b{concept_name}\b.*?\b{filler_name}\b'
+                                     rf'|\b{filler_name}\b.*?\b{concept_name}\b',
+                                     sentence, overlapped=True)
+
+            for m in matches:
+                match_list.append((concept_name, filler_name, concept_id, filler_id, m))
+
+    patterns_list = []
+    for match in match_list:
+        splitted_match = match[4].split()
+        if len(splitted_match) < window:
+            index_first_marker = splitted_match.index(match[0])
+            index_second_marker = splitted_match.index(match[1])
+
+            if (index_first_marker + 1 == index_second_marker) or (
+                    index_second_marker + 1 == index_first_marker):
+                direction = '*'
+            elif index_first_marker < index_second_marker:
+                direction = 'L_'
+            elif index_first_marker > index_second_marker:
+                direction = 'R_'
             else:
-                for match in matches:
-                    splitted_match = match.split()
-                    if len(splitted_match) < window:
-                        index_first_marker = splitted_match.index(concept_name)
-                        index_second_marker = splitted_match.index(filler_name)
+                direction = 'N/A'
 
-                        if (index_first_marker + 1 == index_second_marker) or (
-                                index_second_marker + 1 == index_first_marker):
-                            direction = '*'
-                        elif index_first_marker < index_second_marker:
-                            direction = 'L_'
-                        elif index_first_marker > index_second_marker:
-                            direction = 'R_'
-                        else:
-                            direction = 'N/A'
+            pattern_text = ' '.join(
+                filter(lambda word: word != splitted_match[index_first_marker] and word != splitted_match[
+                    index_second_marker], splitted_match))
 
-                        pattern_text = ' '.join(
-                            filter(lambda word: word != splitted_match[index_first_marker] and word != splitted_match[
-                                index_second_marker], splitted_match))
-
-                        patterns_list.append(
-                            (direction, concept_name, filler_name, concept_id, filler_id, pattern_text))
+            patterns_list.append((direction, match[0], match[1], match[2], match[3], pattern_text))
 
     patterns_list = list(set(patterns_list))
+    patterns_list = sorted(patterns_list, key=lambda item: (item[1], item[2], item[3], item[4]))
 
-    patterns_list = sorted(patterns_list, key=lambda item: (
-        item[1], item[2], item[3], item[4]))
+    grouped_patterns_list = defaultdict(lambda: defaultdict(list))
+    for key, group in itertools.groupby(patterns_list, key=lambda item: (item[3], item[4])):
+        for key_2, inner_group in itertools.groupby(list(group), key=lambda elem: (elem[1], elem[2])):
+            grouped_patterns_list[key][key_2] = list(inner_group)
 
-    grouped_patterns_list = [(key, list(group)) for key, group in
-                             itertools.groupby(patterns_list, key=lambda item: (
-                                 item[1], item[2], item[3], item[4]))]
+    for bsyn_group in grouped_patterns_list:
+        ids = etree.SubElement(pattern_root, 'ids',
+                               concept_babelsynset=bsyn_group[0], filler_babelsynset=bsyn_group[1])
 
-    for group in grouped_patterns_list:
-        markers = etree.SubElement(pattern_root, "markers", concept=group[0][0], filler=group[0][1],
-                                   concept_babelsynset=group[0][2], filler_babelsynset=group[0][3])
+        for text_group in grouped_patterns_list[bsyn_group]:
+            markers = etree.SubElement(ids, 'markers',
+                                       concept_name=text_group[0], filler_name=text_group[1])
 
-        for pattern in group[1]:
-            marker_child = etree.SubElement(markers, "pattern", direction=pattern[0])
-            marker_child.text = pattern[5]
+            for pattern in grouped_patterns_list[bsyn_group][text_group]:
+                marker_child = etree.SubElement(markers, "pattern", direction=pattern[0])
+                marker_child.text = pattern[5]
 
-        markers[:] = sorted(markers, key=lambda child_pattern: child_pattern.get('direction'))
+            markers[:] = sorted(markers, key=lambda child_pattern: child_pattern.get('direction'))
 
     with open(f'{file}_patterns.xml', mode='wb') as pattern_file:
         pattern_file.write(
@@ -170,6 +201,7 @@ def get_lemmas_from_babelsynset(synset: str, key: str, search_lang: str = 'EN', 
     """
     # key = 'd98e5389-2438-4db4-8672-fcdd4ce6d4f9'    # key1
     # key2 = '34461ea6-4c3a-411f-9531-d9e3cae24954'   # key2
+    # 8e149252-ac13-4e51-96ba-e7055f6d56e2
 
     import urllib
     import urllib3
@@ -241,14 +273,7 @@ def create_babelnet_dict():
         pickle.dump(babelnet_dict, output_file, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-with open('babel_dict.pkl', mode='rb') as inp:
-    babelnet_dict = pickle.load(inp)
-
-for k in babelnet_dict:
-    print(k, babelnet_dict[k])
-
-
 # create_babelnet_dict()
-# get_patterns()
-# get_lemmas_from_babelsynset('bn:00043021n', key='d98e5389-2438-4db4-8672-fcdd4ce6d4f9')
+get_patterns()
+# print(get_lemmas_from_babelsynset('bn:00007406n', key='8e149252-ac13-4e51-96ba-e7055f6d56e2'))
 # get_babelsynsets_from_lemmas('aircraft', key='d98e5389-2438-4db4-8672-fcdd4ce6d4f9')
